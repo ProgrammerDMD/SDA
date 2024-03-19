b94fb9c8946c79ad27edad82306d43157b9c9d6d
ProgrammerDMD <50269049+ProgrammerDMD@users.noreply.github.com>
2024-03-19 11:53:06 +0200

    Laborator 3 added elementary functions

--- Laborator 3/main.c
+++ Laborator 3/main.c
@@ -11,7 +11,11 @@ typedef struct List {
     Node* head;
 } List;
 
-Node* createNode(int data) {
+Node* createNode() {
+    int data;
+    printf("Care este marimea acestui nod? ");
+    scanf("%d", &data);
+
     Node* node = malloc(sizeof(Node));
     node->data = data;
     node->left = NULL;
@@ -19,23 +23,43 @@ Node* createNode(int data) {
     return node;
 }
 
-Node* addNode(Node* head, int data) {
+Node* addNode(Node* head) {
     if (head->right == NULL) {
-        Node* node = createNode(data);
-        head->right = createNode(data);
+        Node* node = createNode();
+        head->right = node;
         return node;
     }
 
     if (head->left == NULL) {
-        Node* node = createNode(data);
-        head->left = createNode(data);
+        Node* node = createNode();
+        head->left = node;
         return node;
     }
 
-    return addNode(head->right, data);
+    return addNode(head->right);
+}
+
+void readNode(List* list) {
+    system("cls");
+    printf("Cate elemente doresti sa citesti? ");
+
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 1 && list->head == NULL) {
+        n -= 1;
+        Node* node = createNode();
+        list->head = node;
+    }
+
+    for (int i = 0; i < n; i++) {
+        addNode(list->head);
+    }
 }
 
 void displayNodes(Node* head) {
+    if (head == NULL) return;
+
     printf("%d ", head->data);
     if (head->right != NULL) {
         displayNodes(head->right);
@@ -46,20 +70,37 @@ void displayNodes(Node* head) {
     }
 }
 
+int meniu() {
+    system("cls");
+
+    printf("Selecteaza o optiune:\n");
+    printf("0. Iesire din program\n");
+    printf("1. Citeste de la tastatura unul sau mai multe elemente\n");
+    printf("2. Afiseaza elementele arborelui\n");
+
+    int optiune;
+    scanf("%d", &optiune);
+
+    return optiune;
+}
+
 int main() {
 
     List* list = malloc(sizeof(List));
+    list->head = NULL;
 
-    Node* head = createNode(0);
-    list->head = head;
-
-    addNode(head, 1);
-    addNode(head, 2);
-    addNode(head, 3);
-    addNode(head, 4);
-    addNode(head, 5);
-
-    displayNodes(head);
+    int optiune;
+    while ((optiune = meniu())) {
+        if (optiune == 1) {
+            readNode(list);
+        }
+        if (optiune == 2) {
+            system("cls");
+            displayNodes(list->head);
+            printf("\n");
+            system("pause");
+        }
+    }
 
     return 0;
 }
